fa400539ec25fe1912cf46e14498cf69f8cd0c8f
TKtheDEV <legendzmail@proton.me>
2025-08-04 18:05:28 +0200

    added switch
    Signed-off-by: TKtheDEV <legendzmail@proton.me>

--- target/linux/realtek/image/rtl931x.mk
+++ target/linux/realtek/image/rtl931x.mk
@@ -1,1 +1,9 @@
 # SPDX-License-Identifier: GPL-2.0-only
+
+define Device/hasivo_s1300wp-8xgt-4s-plus
+  DEVICE_TITLE := Hasivo S1300WP-8XGT-4S+
+  DEVICE_DTS   := rtl9310_hasivo_s1300wp-8xgt-4s-plus
+  DEVICE_PACKAGES := kmod-leds-gpio
+  IMAGE_SIZE := 7340032
+endef
+TARGET_DEVICES += hasivo_s1300wp-8xgt-4s-plus
