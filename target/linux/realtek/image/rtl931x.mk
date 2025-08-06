7fb0aa6503f2582ba8dae02194f904297d2528c0
TKtheDEV <legendzmail@proton.me>
2025-08-06 17:09:59 +0200

    typo
    Signed-off-by: TKtheDEV <legendzmail@proton.me>

--- target/linux/realtek/image/rtl931x.mk
+++ target/linux/realtek/image/rtl931x.mk
@@ -2,7 +2,7 @@
 
 define Device/hasivo_s1300wp-8xgt-4s-plus
   DEVICE_TITLE := Hasivo S1300WP-8XGT-4S+
-  DEVICE_DTS   := rtl9310_hasivo_s1300wp-8xgt-4s-plus
+  DEVICE_DTS   := rtl9313_hasivo_s1300wp-8xgt-4s-plus
   DEVICE_PACKAGES := kmod-leds-gpio
   IMAGE_SIZE := 7340032
 endef
